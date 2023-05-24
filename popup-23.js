d662941e025a0de3a0d872d943eeaea22d04e5ad
realmag777 <realmag777@gmail.com>
2023-05-25 01:13:32 +0200

    Add files via upload

--- popup-23.js
+++ popup-23.js
@@ -12,13 +12,13 @@
  *   MIT license - https://en.wikipedia.org/wiki/MIT_License
  */
 'use strict';
-//25-05-2023 modificated
+//25-05-2023
 //1 object == 1 popup
 class Popup23 {
 
     constructor(data = {}) {
         if (typeof Popup23.z_index === 'undefined') {
-            Popup23.z_index = 15003;
+            Popup23.z_index = 15001;
         }
 
         ++Popup23.z_index;
@@ -32,14 +32,13 @@ class Popup23 {
         this.node.className = 'popup23-dynamic-popup-wrapper';
         this.node.innerHTML = this.get_template();
         document.querySelector('body').appendChild(this.node);
-        this.node.querySelector('.popup23-modal').style.zIndex = Popup23.z_index;
-        this.node.querySelector('.popup23-modal-backdrop').style.zIndex = Popup23.z_index - 1;
+        this.node.querySelector('.popup23').style.zIndex = Popup23.z_index;
+        this.node.querySelector('.popup23-backdrop').style.zIndex = Popup23.z_index - 1;
 
-        this.node.querySelectorAll('.popup23-modal-close, .popup23-modal-button-close').forEach(item => {
+        this.node.querySelectorAll('.popup23-close, .popup23-footer-button-close').forEach(item => {
             item.addEventListener('click', e => {
                 e.preventDefault();
                 e.stopPropagation();
-
                 this.node.remove();
                 return false;
             });
@@ -79,19 +78,19 @@ class Popup23 {
         }
 
         if (typeof data.width !== 'undefined') {
-            this.node.querySelector('.popup23-modal').style.maxWidth = data.width + 'px';
+            this.node.querySelector('.popup23').style.maxWidth = data.width + 'px';
         }
 
         if (typeof data.height !== 'undefined') {
-            this.node.querySelector('.popup23-modal').style.maxHeight = data.height + 'px';
+            this.node.querySelector('.popup23').style.maxHeight = data.height + 'px';
         }
 
         if (typeof data.left !== 'undefined') {
-            this.node.querySelector('.popup23-modal').style.left = data.left + '%';
+            this.node.querySelector('.popup23').style.left = data.left + '%';
         }
 
         if (typeof data.left !== 'undefined') {
-            this.node.querySelector('.popup23-modal').style.right = data.right + '%';
+            this.node.querySelector('.popup23').style.right = data.right + '%';
         }
 
         if (typeof data.action !== 'undefined' && data.action.length > 0) {
@@ -101,7 +100,7 @@ class Popup23 {
 
         //***
 
-        this.node.querySelector('.popup23-modal-inner-content').addEventListener('scroll', (e) => {
+        this.node.querySelector('.popup23-inner-content').addEventListener('scroll', (e) => {
             document.dispatchEvent(new CustomEvent('popup23-scrolling', {
                 detail: {
                     top: e.srcElement.scrollTop,
@@ -119,23 +118,23 @@ class Popup23 {
 
     get_template() {
         return `
-        <div class="popup23-modal">
-               <div class="popup23-modal-inner">
-                   <div class="popup23-modal-inner-header">
-                       <h3 class="popup23-modal-title">&nbsp;</h3>
-                       <div class="popup23-modal-title-info">&nbsp;</div>
-                       <a href="javascript: void(0);" class="popup23-modal-close"></a>
+        <div class="popup23">
+               <div class="popup23-inner">
+                   <div class="popup23-inner-header">
+                       <h3 class="popup23-title">&nbsp;</h3>
+                       <div class="popup23-title-info">&nbsp;</div>
+                       <a href="javascript: void(0);" class="popup23-close"></a>
                    </div>
-                   <div class="popup23-modal-inner-content">
+                   <div class="popup23-inner-content">
                        <div class="popup23-form-element-container">Loading ...</div>
                    </div>
-                   <div class="popup23-modal-inner-footer">
-                       <a href="javascript: void(0);" class="btn btn-big btn-blue popup23-modal-button-close">Close</a>
+                   <div class="popup23-inner-footer">
+                       <a href="javascript: void(0);" class="button popup23-footer-button-close">Close</a>
                    </div>
                </div>
            </div>
 
-        <div class="popup23-modal-backdrop"></div>
+        <div class="popup23-backdrop"></div>
     `;
     }
 
@@ -152,11 +151,11 @@ class Popup23 {
     }
 
     set_title(title) {
-        this.node.querySelector('.popup23-modal-title').innerHTML = title;
+        this.node.querySelector('.popup23-title').innerHTML = title;
     }
 
     set_title_info(info) {
-        this.node.querySelector('.popup23-modal-title-info').innerHTML = info;
+        this.node.querySelector('.popup23-title-info').innerHTML = info;
     }
 
     set_content(content) {
@@ -169,6 +168,6 @@ class Popup23 {
     }
 
     get_content_area_height() {
-        return this.node.querySelector('.popup23-modal-inner-content').offsetHeight - 50;
+        return this.node.querySelector('.popup23-inner-content').offsetHeight - 50;
     }
 }
