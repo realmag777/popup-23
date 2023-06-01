7a0537a61bbc504f475c6c9df5dbe890303e5918
realmag777 <realmag777@gmail.com>
2023-06-01 15:21:42 +0200

    Add files via upload

--- popup-23.js
+++ popup-23.js
@@ -1,7 +1,7 @@
 /**
  * @summary     Popup23
  * @description popup window
- * @version     1.0.5
+ * @version     1.0.6
  * @file        popup-23
  * @author      realmag777
  * @contact     https://pluginus.net/contact-us/
@@ -12,9 +12,9 @@
  */
 
 'use strict';
-//25-05-2023
+//29-05-2023
 //1 object is 1 popup
-class Popup23 {
+export default class Popup23 {
 
     constructor(data = {}) {
         if (typeof Popup23.z_index === 'undefined') {
@@ -31,13 +31,23 @@ class Popup23 {
         this.data.start_content = data.start_content ?? 'Loading ...';
 
         this.node = document.createElement('div');
-        this.node.setAttribute('id', this.create_id());
+
+        if (typeof data.id === 'undefined') {
+            data.id = this.create_id();
+        }
+        this.node.setAttribute('id', data.id);
+
         this.node.className = 'popup23-wrapper';
         this.node.innerHTML = this.get_template();
 
         document.querySelector('body').appendChild(this.node);
         this.node.querySelector('.popup23').style.zIndex = Popup23.z_index;
-        this.node.querySelector('.popup23-backdrop').style.zIndex = Popup23.z_index - 1;
+
+        if (!this.data.hide_backdrop) {
+            this.node.querySelector('.popup23-backdrop').style.zIndex = Popup23.z_index - 1;
+        } else {
+            this.node.querySelector('.popup23-backdrop').remove();
+        }
 
         this.node.querySelectorAll('.popup23-close, .popup23-footer-button-close').forEach(item => {
             item.addEventListener('click', e => {
@@ -52,29 +62,36 @@ class Popup23 {
 
         this.set_title(data.title);
 
-        if (typeof data.help_title !== 'undefined') {
-            if (typeof data.help_link !== 'undefined') {
-                this.set_title_info(`<a href="${data.help_link}" class="popup23-btn" target="_blank">${data.help_title}</a>`);
-            } else {
-                this.set_title_info(data.help_title);
-            }
+        if (typeof data.help_info !== 'undefined') {
+            this.set_title_info(data.help_info);
         }
 
         if (typeof data.width !== 'undefined') {
-            this.node.querySelector('.popup23').style.maxWidth = data.width + 'px';
+            this.node.querySelector('.popup23').style.maxWidth = data.width;
+            this.node.querySelector('.popup23').style.minWidth = data.width;
         }
 
         if (typeof data.height !== 'undefined') {
-            this.node.querySelector('.popup23').style.maxHeight = data.height + 'px';
+            this.node.querySelector('.popup23').style.maxHeight = data.height;
+            this.node.querySelector('.popup23').style.minHeight = data.height;
         }
 
         if (typeof data.left !== 'undefined') {
-            this.node.querySelector('.popup23').style.left = data.left + '%';
+            this.node.querySelector('.popup23').style.left = data.left;
         }
 
         if (typeof data.right !== 'undefined') {
-            this.node.querySelector('.popup23').style.right = data.right + '%';
+            this.node.querySelector('.popup23').style.right = data.right;
         }
+
+        if (typeof data.top !== 'undefined') {
+            this.node.querySelector('.popup23').style.top = data.top;
+        }
+
+        if (typeof data.bottom !== 'undefined') {
+            this.node.querySelector('.popup23').style.bottom = data.bottom;
+        }
+
 
         if (typeof data.action !== 'undefined' && data.action.length > 0) {
             document.dispatchEvent(new CustomEvent(data.action, {detail: {...data, ... {popup: this}}}));
@@ -89,6 +106,66 @@ class Popup23 {
             }));
         });
 
+        //***
+        if (typeof data.mousemove !== 'undefined') {
+
+            let can_move = false;
+            let header = this.node.querySelector('.popup23-header');
+            header.onmouseover = () => header.style.cursor = 'move';
+
+            header.addEventListener('mousedown', e => {
+                can_move = true;
+            });
+
+            document.addEventListener('mouseup', e => {
+                can_move = false;
+            });
+
+
+            let position = localStorage.getItem(data.id) ? JSON.parse(localStorage.getItem(data.id)) : {};
+            if (position.left) {
+                this.node.querySelector('.popup23').style.setProperty('left', position.left);
+            }
+
+            if (position.top) {
+                this.node.querySelector('.popup23').style.setProperty('top', position.top);
+            }
+
+            let timer = null;
+
+            //+++
+
+            let prev_screenX = -1;
+            let prev_screenY = -1;
+            document.addEventListener('mousemove', e => {
+
+                if (can_move) {
+
+                    if (prev_screenX !== -1 && e.which === 1 && e.clientX > 0) {
+                        let left = this.node.querySelector('.popup23').style.left;
+                        let diff = parseInt(e.screenX) - prev_screenX;
+                        position.left = `calc(${left} + ${diff}px)`;
+                        this.node.querySelector('.popup23').style.setProperty('left', position.left);
+                    }
+
+                    if (prev_screenY !== -1 && e.which === 1 && e.clientY > 0) {
+                        let top = this.node.querySelector('.popup23').style.top;
+                        let diff = parseInt(e.screenY) - prev_screenY;
+                        position.top = `calc(${top} + ${diff}px)`;
+                        this.node.querySelector('.popup23').style.setProperty('top', position.top);
+                    }
+
+                    prev_screenX = parseInt(e.screenX);
+                    prev_screenY = parseInt(e.screenY);
+                    if (timer) {
+                        clearInterval(timer);
+                    }
+                    timer = setTimeout(() => localStorage.setItem(data.id, JSON.stringify(position)), 777);
+
+                }
+
+            });
+        }
         //***
 
         return this.node;
@@ -129,7 +206,13 @@ class Popup23 {
     }
 
     set_title_info(info) {
-        this.node.querySelector('.popup23-title-info').innerHTML = info;
+        let container = this.node.querySelector('.popup23-title-info');
+        container.innerHTML = '';
+        if (typeof info === 'object') {
+            container.appendChild(info);
+        } else {
+            container.innerHTML = info;
+        }
     }
 
     set_content(content) {
